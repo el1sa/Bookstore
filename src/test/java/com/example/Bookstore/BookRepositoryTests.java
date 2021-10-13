5ed857e50951de30293f0fc69e5cdf11d74308f0
el1sa <elisa.tuovila@myy.haaga-helia.fi>
2021-10-14 01:14:52 +0300

    bookrepositoryn jpatest annotaatio muutettu

--- src/test/java/com/example/Bookstore/BookRepositoryTests.java
+++ src/test/java/com/example/Bookstore/BookRepositoryTests.java
@@ -7,18 +7,15 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.CategoryRepository;
 
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest
 public class BookRepositoryTests {
 	@Autowired
 	private BookRepository repository;
